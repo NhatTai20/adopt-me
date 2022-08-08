08c13fdf8732fa2ccb615d8e7f3d7e2792b525f0
NhatTai2011 <tai.vu3@tiki.vn>
2022-08-09 00:42:24 +0700

    adding typescript

--- src/Details.tsx
+++ src/Details.tsx
@@ -1,22 +1,33 @@
-import { Component, useContext, lazy } from "react";
+import { Component } from "react";
 import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
-const Modal = lazy(() => import('./Modal'));
-class Details extends Component {
-  state = { loading: true, showModal: false };
+import Modal from "./Modal";
+import { PetAPIResponse, Animal } from "./APIResponsesType";
+
+class Details extends Component<{ params: { id?: string }}> {
+  state = {
+    loading: true,
+    showModal: false,
+    animal: "" as Animal,
+    breed: "",
+    city: "",
+    state: "",
+    description: "",
+    name: "",
+    images: [] as string[],
+  };
 
   async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
     );
-    const json = await res.json();
+    const json = (await res.json()) as PetAPIResponse;
     this.setState(Object.assign({ loading: false }, json.pets[0]));
   }
-
   toggleModal = () => this.setState({ showModal: !this.state.showModal });
-
+  adopt = () => (window.location.href = "http://bit.ly/pet-adopt");
   render() {
     if (this.state.loading) {
       return <h2>loading … </h2>;
@@ -31,17 +42,21 @@ class Details extends Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
-          <button
-            onClick={this.toggleModal}
-            style={{ backgroundColor: this.props.theme }}
-          >
-            Adopt {name}
-          </button>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
           {showModal ? (
             <Modal>
               <div>
-                <h1>Would you like to adopt {name}</h1>
+                <h1>Would you like to adopt {name}?</h1>
                 <div className="buttons">
                   <a href="https://bit.ly/pet-adopt">Yes</a>
                   <button onClick={this.toggleModal}>No</button>
@@ -56,11 +71,10 @@ class Details extends Component {
 }
 
 const WrappedDetails = () => {
-  const params = useParams();
-  const [theme] = useContext(ThemeContext);
+  const params = useParams<{ id: string }>();
   return (
     <ErrorBoundary>
-      <Details params={params} theme={theme} />
+      <Details params={params} />
     </ErrorBoundary>
   );
 };
